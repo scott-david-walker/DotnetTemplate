022e55397f457b6f323dd78c67cd609cc09fe09d
Scott Walker <scott.david.walker@hotmail.com>
2025-01-02 18:24:18 +0100

    refactor: cleanup startup by extracting methods

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,69 +1,28 @@
-using System.Reflection;
 using Api;
-using Api.Behaviours;
 using Api.Controllers.Framework;
-using Api.Framework;
-using Api.Settings;
 using Core;
 using Core.Entities;
-using FluentValidation;
-using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 builder.Services.AddAuthorization();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorisationBehaviour<,>));
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-
-builder.Services.Scan(scan => scan
-    .FromAssemblyOf<IAuthoriser>()
-    .AddClasses(classes => classes.AssignableTo<IAuthoriser>())
-    .AsImplementedInterfaces()
-    .WithTransientLifetime());
-
+builder.Services.ConfigureMediatrBehaviours();
+builder.Services.ConfigureAuth(builder.Configuration);
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql("Host=localhost;Database=database;Username=postgres;Password=password;Include Error Detail = true"));
+
 builder.Services.AddIdentityApiEndpoints<User>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddScoped<ICurrentUser, CurrentUser>();
-var jwtSettings =
-    builder.Services.ConfigureSettings<JwtSettings>(builder.Configuration, nameof(JwtSettings));
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(jwtSettings.GetKey())
-        };
-    });
 
 var app = builder.Build();
 
--- Api/StartupExtensions.cs
+++ Api/StartupExtensions.cs
@@ -1,10 +1,18 @@
+using System.Reflection;
+using Api.Behaviours;
+using Api.Framework;
+using Api.Settings;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api;
 
 public static class StartupExtensions
 {
-    public static TSettings ConfigureSettings<TSettings>(this IServiceCollection services,
+    private static TSettings ConfigureSettings<TSettings>(this IServiceCollection services,
         IConfiguration configuration,
         string configurationSectionKey) where TSettings : class, new()
     {
@@ -13,5 +21,46 @@ public static class StartupExtensions
         services.Configure<TSettings>(settings);
         services.AddSingleton(resolver => resolver.GetRequiredService<IOptions<TSettings>>().Value);
         return settings.Get<TSettings>()!;
+    }
+
+    public static IServiceCollection ConfigureMediatrBehaviours(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.Scan(scan => scan
+            .FromAssemblyOf<IAuthoriser>()
+            .AddClasses(classes => classes.AssignableTo<IAuthoriser>())
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorisationBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureAuth(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        var jwtSettings =
+            services.ConfigureSettings<JwtSettings>(configuration, nameof(JwtSettings));
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(jwtSettings.GetKey())
+                };
+            });
+        return services;
     }
 }
